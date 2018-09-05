89fcff5dc1f0a78140950bece65e980afd333f25
VT <vivekanand.thite@synerzip.com>
2018-09-05 15:37:53 +0530

    Add mustache js for message templating

--- public/js/index.js
+++ public/js/index.js
@@ -8,14 +8,27 @@ socket.on('disconnect' , function () {
 });
 
 socket.on('newMessage' , function (message) {
+/* 
+
     var formattedTime = moment(message.createdAt).format('h:mm a');
     console.log('New message',message);
     var li = jQuery('<li></li>');
     li.text(message.from +' ' + formattedTime +':' +message.text);
     jQuery('#messages').append(li);
+
+     */
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template,{
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message){
+/* 
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a= jQuery('<a target="_blank">My current location</a>');
@@ -23,6 +36,16 @@ socket.on('newLocationMessage', function(message){
     a.attr('href',message.url);
     li.append(a);
     jQuery('#messages').append(li); 
+
+     */
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template,{
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 /* 
